e0c25f2dad3823fd9d2dff3d76b0c331d1e8bb04
alex <alex@nabu.be>
2014-06-25 15:42:46 +0200

    Allow you to choose your own backend for the dynamic resource (mainly for large files)

--- src/main/java/be/nabu/libs/resources/DynamicResource.java
+++ src/main/java/be/nabu/libs/resources/DynamicResource.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import be.nabu.libs.resources.api.ReadableResource;
 import be.nabu.libs.resources.api.ResourceContainer;
 import be.nabu.utils.io.api.ByteBuffer;
+import be.nabu.utils.io.api.Container;
+import be.nabu.utils.io.api.DuplicatableContainer;
+import be.nabu.utils.io.api.MarkableContainer;
 import be.nabu.utils.io.api.ReadableContainer;
 import be.nabu.utils.io.buffers.bytes.DynamicByteBuffer;
 import be.nabu.utils.io.containers.ReadableContainerDuplicator;
@@ -20,18 +23,31 @@ public class DynamicResource implements ReadableResource {
 	private ReadableContainer<ByteBuffer> originalContent;
 	private String contentType;
 	private String name;
-	private DynamicByteBuffer content;
+	private Container<ByteBuffer> content;
 	private boolean alreadyRequested = false;
 	private boolean shouldClose = false;
 	
 	@SuppressWarnings("unchecked")
-	public DynamicResource(ReadableContainer<ByteBuffer> content, String name, String contentType, boolean shouldClose) {
+	public <T extends DuplicatableContainer<ByteBuffer, ? extends ReadableContainer<ByteBuffer>> & Container<ByteBuffer>> DynamicResource(ReadableContainer<ByteBuffer> originalContent, T backend, String name, String contentType, boolean shouldClose) {
 		this.name = name;
 		this.contentType = contentType;
-		this.content = new DynamicByteBuffer();
 		this.shouldClose = shouldClose;
+		this.content = backend;
+		if (backend instanceof MarkableContainer)
+			((MarkableContainer<ByteBuffer>) backend).mark();
 		originalContent = new ReadableContainerDuplicator<ByteBuffer>(content, this.content);
-		this.content.mark();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public DynamicResource(ReadableContainer<ByteBuffer> content, String name, String contentType, boolean shouldClose) {
+		this(content, new DynamicByteBuffer(), name, contentType, shouldClose);
+		this.name = name;
+		this.contentType = contentType;
+		this.shouldClose = shouldClose;
+		DynamicByteBuffer buffer = new DynamicByteBuffer();
+		buffer.mark();
+		this.content = buffer;
+		originalContent = new ReadableContainerDuplicator<ByteBuffer>(content, this.content);
 	}
 	
 	@Override
@@ -49,6 +65,7 @@ public class DynamicResource implements ReadableResource {
 		return null;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public ReadableContainer<ByteBuffer> getReadable() throws IOException {
 		if (!alreadyRequested) {
@@ -56,7 +73,7 @@ public class DynamicResource implements ReadableResource {
 			return shouldClose ? originalContent : new UncloseableReadableContainer(originalContent);
 		}
 		else
-			return content.clone();
+			return ((DuplicatableContainer<ByteBuffer, ? extends ReadableContainer<ByteBuffer>>) content).duplicate(true);
 	}
 
 	private static class UncloseableReadableContainer implements ReadableContainer<ByteBuffer> {
