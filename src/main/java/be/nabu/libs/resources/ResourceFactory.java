1aac29ae84295a4ee6e57bd9255a57232ac38aba
alex <alex@nabu.be>
2015-01-12 10:22:04 +0100

    Added some better multi-thread support for initial startup

--- src/main/java/be/nabu/libs/resources/ResourceFactory.java
+++ src/main/java/be/nabu/libs/resources/ResourceFactory.java
@@ -19,8 +19,13 @@ public class ResourceFactory {
 	private static ResourceFactory instance;
 	
 	public static ResourceFactory getInstance() {
-		if (instance == null)
-			instance = new ResourceFactory();
+		if (instance == null) {
+			synchronized(ResourceFactory.class) {
+				if (instance == null) {
+					instance = new ResourceFactory();
+				}
+			}
+		}
 		return instance;
 	}
 	
@@ -52,34 +57,44 @@ public class ResourceFactory {
 	@SuppressWarnings("unchecked")
 	private Map<String, ResourceResolver> getResolvers() {
 		if (resolvers.isEmpty()) {
-			try {
-				// let's try this with custom service loading based on a configuration
-				Class<?> clazz = getClass().getClassLoader().loadClass("be.nabu.utils.services.ServiceLoader");
-				Method declaredMethod = clazz.getDeclaredMethod("load", Class.class);
-				for (ResourceResolver resolver : (List<ResourceResolver>) declaredMethod.invoke(null, ResourceResolver.class)) {
-					addResourceResolver(resolver);
-				}
-			}
-			catch (ClassNotFoundException e) {
-				// ignore, the framework is not present
-			}
-			catch (NoSuchMethodException e) {
-				// corrupt framework?
-				throw new RuntimeException(e);
-			}
-			catch (SecurityException e) {
-				throw new RuntimeException(e);
-			}
-			catch (IllegalAccessException e) {
-				// ignore
-			}
-			catch (InvocationTargetException e) {
-				// ignore
-			}
-			if (resolvers.isEmpty()) {
-				ServiceLoader<ResourceResolver> serviceLoader = ServiceLoader.load(ResourceResolver.class);
-				for (ResourceResolver resolver : serviceLoader) {
-					addResourceResolver(resolver);
+			synchronized(this) {
+				if (resolvers.isEmpty()) {
+					Map<String, ResourceResolver> resolvers = new LinkedHashMap<String, ResourceResolver>();
+					try {
+						// let's try this with custom service loading based on a configuration
+						Class<?> clazz = getClass().getClassLoader().loadClass("be.nabu.utils.services.ServiceLoader");
+						Method declaredMethod = clazz.getDeclaredMethod("load", Class.class);
+						for (ResourceResolver resolver : (List<ResourceResolver>) declaredMethod.invoke(null, ResourceResolver.class)) {
+							for (String scheme : resolver.getDefaultSchemes()) {
+								resolvers.put(scheme, resolver);
+							}
+						}
+					}
+					catch (ClassNotFoundException e) {
+						// ignore, the framework is not present
+					}
+					catch (NoSuchMethodException e) {
+						// corrupt framework?
+						throw new RuntimeException(e);
+					}
+					catch (SecurityException e) {
+						throw new RuntimeException(e);
+					}
+					catch (IllegalAccessException e) {
+						// ignore
+					}
+					catch (InvocationTargetException e) {
+						// ignore
+					}
+					if (resolvers.isEmpty()) {
+						ServiceLoader<ResourceResolver> serviceLoader = ServiceLoader.load(ResourceResolver.class);
+						for (ResourceResolver resolver : serviceLoader) {
+							for (String scheme : resolver.getDefaultSchemes()) {
+								resolvers.put(scheme, resolver);
+							}
+						}
+					}
+					this.resolvers.putAll(resolvers);
 				}
 			}
 		}
@@ -90,7 +105,7 @@ public class ResourceFactory {
 		// it is possible to return null so for instance you might want to check if something exists and if not, create it
 		// the "mkdir()" functionality has the ability to scan further up the tree to find something
 		if (getResolvers().containsKey(uri.getScheme()))
-			return resolvers.get(uri.getScheme()).getResource(uri, principal);
+			return getResolvers().get(uri.getScheme()).getResource(uri, principal);
 		else
 			throw new IllegalArgumentException("The scheme " + uri.getScheme() + " has no registered handler");
 	}
