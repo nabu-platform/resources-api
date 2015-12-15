0d0c24a277fa0f256ad13dfadf4f28aedb8f06d9
alex <alex@nabu.be>
2015-12-15 14:17:29 +0100

    Added a combined container

---
+++ src/main/java/be/nabu/libs/resources/CombinedContainer.java
@@ -0,0 +1,84 @@
+package be.nabu.libs.resources;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+import be.nabu.libs.resources.api.Resource;
+import be.nabu.libs.resources.api.ResourceContainer;
+
+public class CombinedContainer<T extends Resource> implements ResourceContainer<T>, Closeable {
+
+	private List<ResourceContainer<T>> containers = new ArrayList<ResourceContainer<T>>();
+	private ResourceContainer<?> parent;
+	private String name;
+	
+	@SuppressWarnings("unchecked")
+	public CombinedContainer(ResourceContainer<?> parent, String name, ResourceContainer<?>...containers) {
+		this.parent = parent;
+		this.name = name;
+		if (containers != null) {
+			this.containers.addAll((Collection<? extends ResourceContainer<T>>) Arrays.asList(containers));
+		}
+	}
+	
+	@Override
+	public String getContentType() {
+		return Resource.CONTENT_TYPE_DIRECTORY;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public ResourceContainer<?> getParent() {
+		return parent;
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		final List<ResourceContainer<T>> containersToLoop = new ArrayList<ResourceContainer<T>>(containers);
+		return new Iterator<T>() {
+			private Iterator<T> current;
+			@Override
+			public boolean hasNext() {
+				if (current == null || !current.hasNext()) {
+					current = containersToLoop.size() > 0 ? containersToLoop.remove(0).iterator() : null;
+				}
+				return current != null;
+			}
+
+			@Override
+			public T next() {
+				if (current == null || !current.hasNext()) {
+					current = containersToLoop.size() > 0 ? containersToLoop.remove(0).iterator() : null;
+				}
+				return current == null ? null : current.next();
+			}
+		};
+	}
+
+	@Override
+	public T getChild(String name) {
+		for (ResourceContainer<T> container : containers) {
+			T child = container.getChild(name);
+			if (child != null) {
+				return child;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public void close() throws IOException {
+		for (ResourceContainer<T> container : containers) {
+			ResourceUtils.close(container);
+		}
+	}
+}
